699490a0ec3a162160a47c6423670189f6effb40
Ira Horecka <ira89@icloud.com>
2019-10-24 22:56:33 -0700

    General upkeep + addition of requirements.txt

--- Primerwalk.py
+++ Primerwalk.py
@@ -1,99 +1,90 @@
-#2019-07-22: script to generate primer walk for large DNA sequence - order form for IDT generated
-#TO_DO: Add both plate and tube IDT order generator. 
-#Communicates with frontend file for GUI interface (Primerwalk_GUI.py)
-#Update: Now reads .csv file 'sequence_file.csv' for input - bulk primer synthesis capacity in place with new directory generated per day.
-#   name of vector to be synthesized
-#   full vector sequence string and segment of interest (NT) whether uppercase or lowercase, does not matter
-#   bp break you would like per primer generation (common is 300 bp)
-#   nM concentration of primer to be ordered on IDT
+"""
+Backend .py file for Primerwalk_GUI.py
+This script generates appropriate forward and reverse
+primers for sequences included in the imported .csv
+file (sequence_file.csv) and determines tube or 96-plate
+layout for ordering on IDT.
+"""
 
-#   Primer selction criteria available on: https://dnacore.mgh.harvard.edu/new-cgi-bin/site/pages/sequencing_pages/primer_design.jsp;jsessionid=633F7917850FBFDF707F67F872267D71 
-#   IDT website for oligo synthesis: https://www.idtdna.com/site/order/oligoentry
-
-import csv 
+import csv
 import datetime
-import pandas as pd
 import os.path
 import shutil
+import pandas as pd
 import Primerwalk_GUI
 
 global order_type
 
-seq_table = {
-    'A':'T',
-    'G':'C',
-    'C':'G',
-    'T':'A'
+SEQ_TABLE = {
+    'A': 'T',
+    'G': 'C',
+    'C': 'G',
+    'T': 'A'
 }
 
-well_list = ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 
-'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 
-'A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3', 
-'A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4', 
-'A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5', 
-'A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6', 
-'A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7', 
-'A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8', 
-'A9', 'B9', 'C9', 'D9', 'E9', 'F9', 'G9', 'H9', 
-'A10', 'B10', 'C10', 'D10', 'E10', 'F10', 'G10', 'H10', 
-'A11', 'B11', 'C11', 'D11', 'E11', 'F11', 'G11', 'H11', 
-'A12', 'B12', 'C12', 'D12', 'E12', 'F12', 'G12', 'H12']
+WELL_LIST = ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1',
+             'A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2',
+             'A3', 'B3', 'C3', 'D3', 'E3', 'F3', 'G3', 'H3',
+             'A4', 'B4', 'C4', 'D4', 'E4', 'F4', 'G4', 'H4',
+             'A5', 'B5', 'C5', 'D5', 'E5', 'F5', 'G5', 'H5',
+             'A6', 'B6', 'C6', 'D6', 'E6', 'F6', 'G6', 'H6',
+             'A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7',
+             'A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8',
+             'A9', 'B9', 'C9', 'D9', 'E9', 'F9', 'G9', 'H9',
+             'A10', 'B10', 'C10', 'D10', 'E10', 'F10', 'G10', 'H10',
+             'A11', 'B11', 'C11', 'D11', 'E11', 'F11', 'G11', 'H11',
+             'A12', 'B12', 'C12', 'D12', 'E12', 'F12', 'G12', 'H12']
 
 
-# == FUNCTION ==
-
-#complement function for sequence
+# complement function for sequence
 def complement(seq):
     comp = ''
     for i in seq:
-        comp += seq_table.get(i.upper(), '0')
+        comp += SEQ_TABLE.get(i.upper(), '0')
     return comp
-#reverse complement function for sequence
+
+
+# reverse complement function for sequence
 def r_complement(seq):
     comp = ''
     for i in seq:
-        comp += seq_table.get(i.upper(), '0')
+        comp += SEQ_TABLE.get(i.upper(), '0')
     return comp[::-1]
 
-#checks for secondary primer binding sites
+
+# checks for secondary primer binding sites
 def nonspecific_looker(fprimer, rprimer, full_seq):
     full_seq = full_seq.upper()
     reverse_full = r_complement(full_seq)
     for i in fprimer:
         if full_seq.count(i) > 1 or reverse_full.count(i) > 1:
             return 'FAIL'
-        else:
-            pass
     for i in rprimer:
         if full_seq.count(i) > 1 or reverse_full.count(i) > 1:
             return 'FAIL'
-        else:
-            pass
+
     return 'PASS'
 
 
-# == CLASS  ==
-    
-#write fprimer and rprimer sequences to .csv file compatable with IDT oligoDNA bulk input
-class IDT_CSV:
+# write fprimer and rprimer sequences to .csv file compatable with IDT oligoDNA bulk input
+class IDTCSV:
     def __init__(self, fprimer_l, rprimer_l, filename):
         self.fprimer_l = fprimer_l
         self.rprimer_l = rprimer_l
         self.filename = filename
         self.csv_tube_name = f'IDT_Tube_{filename}'
         self.csv_plate_name = f'IDT_Plate_{filename}'
-        self.primer_conc = primer_conc
         self.title = f"{str(self.filename)}.csv"
         self.idt_tube_list = ['Name,Sequence,Scale,Purification']
         self.idt_plate_list = ['Well Position,Name,Sequence']
 
     def write_tube_order(self, primer_conc):
         for i in self.fprimer_l:
-            self.idt_tube_list.append(f"{f'{self.filename}_{self.fprimer_l.index(i)+1}_f'},{i},{self.primer_conc}nm,STD \n")
+            self.idt_tube_list.append(f"{f'{self.filename}_{self.fprimer_l.index(i)+1}_f'},{i},{primer_conc}nm,STD \n")
         for i in self.rprimer_l:
-            self.idt_tube_list.append(f"{f'{self.filename}_{self.rprimer_l.index(i)+1}_r'},{i},{self.primer_conc}nm,STD \n")
-        with open(self.title,'w',newline = '') as csv_tube_file:
-            writer = csv.writer(csv_tube_file, delimiter = ',')
+            self.idt_tube_list.append(f"{f'{self.filename}_{self.rprimer_l.index(i)+1}_r'},{i},{primer_conc}nm,STD \n")
+        with open(self.title, 'w', newline='') as csv_tube_file:
+            writer = csv.writer(csv_tube_file, delimiter=',')
             writer.writerows([i.split(',') for i in self.idt_tube_list])
         csv_tube_file.close()
 
@@ -101,99 +92,80 @@ class IDT_CSV:
         count = -1
         for i in self.fprimer_l:
             count += 1
-            self.idt_plate_list.append(f"{well_list[count]},{f'{self.filename}_{self.fprimer_l.index(i)+1}_f'},{i} \n")
+            self.idt_plate_list.append(f"{WELL_LIST[count]},{f'{self.filename}_{self.fprimer_l.index(i)+1}_f'},{i} \n")
         for i in self.rprimer_l:
             count += 1
-            self.idt_plate_list.append(f"{well_list[count]},{f'{self.filename}_{self.rprimer_l.index(i)+1}_r'},{i} \n")
-        with open(self.title,'w',newline = '') as csv_plate_file:
-            writer = csv.writer(csv_plate_file, delimiter = ',')
+            self.idt_plate_list.append(f"{WELL_LIST[count]},{f'{self.filename}_{self.rprimer_l.index(i)+1}_r'},{i} \n")
+        with open(self.title, 'w', newline='') as csv_plate_file:
+            writer = csv.writer(csv_plate_file, delimiter=',')
             writer.writerows([i.split(',') for i in self.idt_plate_list])
         csv_plate_file.close()
 
     def csv_to_excel(self, order_type):
         path = f'{os.getcwd()}/{self.title}'
         if order_type == 'tube':
-            pd.read_csv(path).to_excel(f'{str(self.csv_tube_name)}.xlsx', index = False)
+            pd.read_csv(path).to_excel(f'{str(self.csv_tube_name)}.xlsx', index=False)
         elif order_type == 'plate':
-            pd.read_csv(path).to_excel(f'{str(self.csv_plate_name)}.xlsx', index = False)
+            pd.read_csv(path).to_excel(f'{str(self.csv_plate_name)}.xlsx', index=False)
         os.remove(path)
 
-    #write .csv file for Tm - could be interesting but not in production
-    def write_Tm(self):
-        pass
 
-
-#checking appropriate primer selection rules:
-#1: GC percentage should be no less than 45% and no greater than 55%
-#2: Melt temp of primer should be greater than 50C and less than 65C
-#3: There should be no more than 4 homopolymer sequences in the primer
-#4: The 3' end of every primer should be a 'G' or 'C'
+# checking appropriate primer selection rules:
+# 1: GC percentage should be no less than 45% and no greater than 55%
+# 2: Melt temp of primer should be greater than 50C and less than 65C
+# 3: There should be no more than 4 homopolymer sequences in the primer
+# 4: The 3' end of every primer should be a 'G' or 'C'
 class Rule:
     def __init__(self, prim_sequence):
         self.p_seq = prim_sequence
-    
-    def GC_perc(self):
-        GC = self.p_seq.count('G') + self.p_seq.count('C')
+
+    def gc_perc(self):
+        _gc = self.p_seq.count('G') + self.p_seq.count('C')
         len_seq = len(self.p_seq)
-        if GC > 0.45*(len_seq) and GC < 0.55*(len_seq):
-            return True
-        else:
-            return False
+        return bool(_gc > 0.45*(len_seq) and _gc < 0.55*(len_seq))
 
     def homo_polymer(self):
-        if 'CCCC' in self.p_seq:
-            return False
-        elif 'AAAA' in self.p_seq:
-            return False
-        elif 'GGGG' in self.p_seq:
-            return False
-        elif 'TTTT' in self.p_seq:
-            return False
-        else:
-            return True
+        codon = lambda nt: nt.upper()*4
+        for i in 'agct':
+            if codon(i) in self.p_seq:
+                return False
+        return True
 
     def last_gc(self):
-        if (self.p_seq[-1] == 'G' or self.p_seq[-1] == 'C') and (self.p_seq[-2] == 'G' or self.p_seq[-2] == 'C'):
-            return True
-        else:
-            return False
+        return bool(self.p_seq[-1] == 'G' or self.p_seq[-1] == 'C') and (self.p_seq[-2] == 'G' or self.p_seq[-2] == 'C')
 
-    def TM(self):
-        GC = self.p_seq.count('G') + self.p_seq.count('C')
-        TA = self.p_seq.count('T') + self.p_seq.count('A')
-        Tm = 64.9 + 41*((GC - 16.4)/(GC + TA))
-        if Tm > 50 and Tm < 65:
-            return True 
-        else:
-            return False
+    def temp_melt(self):
+        gc = self.p_seq.count('G') + self.p_seq.count('C')
+        ta = self.p_seq.count('T') + self.p_seq.count('A')
+        temp_melt = 64.9 + 41*((gc - 16.4)/(gc + ta))
+        return bool(temp_melt > 50 and temp_melt < 65)
 
 
-class Primer_Check:
+class PrimerCheck:
     def __init__(self, seq, count):
         self.seq = seq
         self.count = count
 
-    #generates appropriate 22bp forward primer with test cases in Rule class for each instance of potential forward primer
+    # generates appropriate 22bp forward primer with test cases in Rule class for each instance of potential forward primer
     def f_check(self, fprimer_l):
         fprimer = self.seq[self.count-50:self.count-28].upper()
         fp_test = Rule(fprimer)
         try:
-            if fp_test.GC_perc() and fp_test.TM() and fp_test.homo_polymer() and fp_test.last_gc() == True:
+            if fp_test.gc_perc() and fp_test.temp_melt() and fp_test.homo_polymer() and fp_test.last_gc():
                 fprimer_l.append(fprimer)
                 return fprimer_l
-            else:
-                self.count -= 1
-                self.f_check(fprimer_l)
+            self.count -= 1
+            self.f_check(fprimer_l)
         except RecursionError:
             fprimer_l.append('FAIL')
             return fprimer_l
 
-    #generates appropriate 22bp reverse primer with test cases in Rule class for each instance of potential reverse primer
+    # generates appropriate 22bp reverse primer with test cases in Rule class for each instance of potential reverse primer
     def r_check(self, rprimer_l):
-        rprimer = r_complement(seq[self.count+28:self.count+50]).upper()
+        rprimer = r_complement(self.seq[self.count+28:self.count+50]).upper()
         rp_test = Rule(rprimer)
         try:
-            if rp_test.GC_perc() and rp_test.TM() and rp_test.homo_polymer() and rp_test.last_gc() == True:
+            if rp_test.gc_perc() and rp_test.temp_melt() and rp_test.homo_polymer() and rp_test.last_gc():
                 rprimer_l.append(rprimer)
                 return rprimer_l
             else:
@@ -204,27 +176,27 @@ class Primer_Check:
             return rprimer_l
 
 
-class Primer_Seek:
+class PrimerSeek:
     def __init__(self, seq, bp):
         self.seq = seq
         self.bp = bp
 
-    #appends appropriate primers (both forward and reverse) to specified list
+    # appends appropriate primers (both forward and reverse) to specified list
     def primer_find(self):
         count = 0
         fprimer_l = []
         rprimer_l = []
-        for i in self.seq:
-            count += 1
+        for count in range(len(self.seq)):
             if count % self.bp == 0:
-                Primer = Primer_Check(seq, count)
+                Primer = PrimerCheck(self.seq, count)
                 fprimer_l.append(Primer.f_check(fprimer_l))
                 rprimer_l.append(Primer.r_check(rprimer_l))
-        fprimer_l = [i for i in fprimer_l if type(i) != list and i != None]
-        rprimer_l = [i for i in rprimer_l if type(i) != list and i != None]
+        primer_l = lambda _direction: [i for i in _direction if not isinstance(i, list) and i is not None]
+        fprimer_l = primer_l(fprimer_l)
+        rprimer_l = primer_l(rprimer_l)
         return fprimer_l, rprimer_l
 
-    #if there is an unsuccessful attempt to make a primer, the search bp value is incremented by 10
+    # if there is an unsuccessful attempt to make a primer, the search bp value is incremented by 10
     def primer_synthesis(self):
         fprimer, rprimer = self.primer_find()
         while 'FAIL' in fprimer or 'FAIL' in rprimer:
@@ -233,40 +205,43 @@ class Primer_Seek:
         return fprimer, rprimer
 
 
-#read input file 'sequence_file.csv' for batch primer sequence generation
-seq_file = pd.read_csv('sequence_file.csv')
-new_direct = f'{os.getcwd()}/{datetime.date.today()}'
-#if the directory path you want to make is not there, make it - override if current directory there.
-if os.path.exists(new_direct):
-    shutil.rmtree(new_direct)
-os.makedirs(new_direct)
-os.chdir(new_direct)
+def process_csv(order_type):
+    # read input file 'sequence_file.csv' for batch primer sequence generation
+    seq_file = pd.read_csv('sequence_file.csv')
+    new_direct = f'{os.getcwd()}/{datetime.date.today()}'
+    # if the directory path you want to make is not there, make it - override if current directory there.
+    if os.path.exists(new_direct):
+        shutil.rmtree(new_direct)
+    os.makedirs(new_direct)
+    os.chdir(new_direct)
 
-for index, row in seq_file.iterrows():
-    filename = row['Name']
-    full_seq = row['Full Sequence']
-    seq = row['Primer Walk']
-    bp = row['BP Gap']
-    primer_conc = row['Primer conc. (nM)']
+    for index, row in seq_file.iterrows():
+        filename = row['Name']
+        full_seq = row['Full Sequence']
+        seq = row['Primer Walk']
+        bp = row['BP Gap']
+        primer_conc = row['Primer conc. (nM)']
 
-    #checks for sequences for every row in 'sequence_file.csv'
-    Seek = Primer_Seek(seq, bp)
-    fprimer, rprimer = Seek.primer_synthesis()
-    status = nonspecific_looker(fprimer, rprimer, full_seq)
-    while status == 'FAIL':
-        bp += 10
-        Seek = Primer_Seek(seq, bp)
+        # checks for sequences for every row in 'sequence_file.csv'
+        Seek = PrimerSeek(seq, bp)
         fprimer, rprimer = Seek.primer_synthesis()
         status = nonspecific_looker(fprimer, rprimer, full_seq)
-    fprimer = list(set(fprimer))
-    rprimer = list(set(rprimer))
+        while status == 'FAIL':
+            bp += 10
+            Seek = PrimerSeek(seq, bp)
+            fprimer, rprimer = Seek.primer_synthesis()
+            status = nonspecific_looker(fprimer, rprimer, full_seq)
+        fprimer = list(set(fprimer))
+        rprimer = list(set(rprimer))
 
-    write_to_file = IDT_CSV(fprimer, rprimer, filename)
-    if order_type.lower() == 'tube':
-        write_to_file.write_tube_order(primer_conc)
-    elif order_type.lower() == 'plate':
-        write_to_file.write_plate_order()
-    write_to_file.csv_to_excel(order_type)
+        write_to_file = IDTCSV(fprimer, rprimer, filename)
+        if order_type.lower() == 'tube':
+            write_to_file.write_tube_order(primer_conc)
+        elif order_type.lower() == 'plate':
+            write_to_file.write_plate_order()
+        write_to_file.csv_to_excel(order_type)
 
-#print to terminal if script executes successfully
+
+
+process_csv(order_type)
 print(f'\n-- COMPLETE --\nPlease check folder: {datetime.date.today()}\n')
--- Primerwalk_GUI.py
+++ Primerwalk_GUI.py
@@ -1,36 +1,38 @@
-#modified 2019-07-22 for primer design front end
-#add classes for button, header, and label colors and sizes [x]
-#figure out a way to include a 'back' button for child app to allow homepage selection [x]
-
+"""
+A .py file to generate kivy user interface to facilitate
+easy primer generation and order placement template for IDT. 
+"""
 import os
 import datetime
-import pandas as pd
 import kivy
-kivy.require("1.10.1")
-
 from kivy.config import Config
-Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
-#Resizing application windows to troubleshoot a kivy interface problem in Windows (use in macOS, as well).
-Config.set('graphics', 'resizable', 0)
-Config.set('graphics', 'borderless', 0)
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.graphics import Rectangle, Color
+kivy.require("1.10.1")
+Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
+Config.set('graphics', 'resizable', 0)
+Config.set('graphics', 'borderless', 0)
+MAIN_DIR = os.path.dirname(os.path.abspath(__file__))
 
-main_dir = '/Users/irahorecka/Desktop/Harddrive_Desktop/Python/Primer Walk'
-status = 'incomplete'
 
 class FontSize:
+    """
+    Set fontsize for Kivy GUI interface.
+    """
     def __init__(self):
         self.header_size = 36
         self.subheader_size = 21
         self.label_size = 17
         self.bttn_size = 25
 
+
 class ColorFormat:
+    """
+    Set color for Kivy GUI font and background
+    """
     def __init__(self):
         self.bttn_bckgrd_color = (0.00, 0.29, 0.52, 1.00)
         self.scrn_bckgrd_color = (1.00, 1.00, 1.00, 1.00)
@@ -38,18 +40,21 @@ class ColorFormat:
         self.bttn_fwd_color = (0.22, 0.66, 0.76, 1.00)
         self.header_color = (0.00, 0.29, 0.52, 1.00)
 
-fontsize = FontSize()
-colorformat = ColorFormat()
 
 class WorklistGenerator(GridLayout):
+    """
+    Define layout for Kivy GUI with button properties
+    """
     def __init__(self, **kwargs):
         super(WorklistGenerator, self).__init__(**kwargs)
-        #adds fixed color to kivy background - it works thank god.
+        self.fontsize = FontSize()
+        self.colorformat = ColorFormat()
+
         with self.canvas:
-            Color(rgba = colorformat.scrn_bckgrd_color)
+            Color(rgba=self.colorformat.scrn_bckgrd_color)
             self.rect = Rectangle(pos=self.pos, size=self.size)
         self.bind(pos=self.update_rect,
-                size=self.update_rect)
+                  size=self.update_rect)
 
         self.inside = GridLayout()
         self.inside.cols = 3
@@ -68,69 +73,81 @@ class WorklistGenerator(GridLayout):
 
         self.cols = 1
 
-        self.inside1.add_widget(Label(text = ""))
-        self.inside2.add_widget(Label(text = ""))
-        self.inside3.add_widget(Label(text = ""))
-        self.inside4.add_widget(Label(text = ""))
+        self.inside1.add_widget(Label(text=""))
+        self.inside2.add_widget(Label(text=""))
+        self.inside3.add_widget(Label(text=""))
+        self.inside4.add_widget(Label(text=""))
 
-        self.close_select = Button(text = "[b]CLOSE[/b]", markup = True, color = colorformat.bttn_bck_color, 
-        background_normal = '', 
-        background_color = colorformat.bttn_bckgrd_color,
-        font_size = fontsize.bttn_size)
-        self.close_select.bind(on_press = self.close)
+        self.close_select = Button(text="[b]CLOSE[/b]", markup=True,
+                                   color=self.colorformat.bttn_bck_color,
+                                   background_normal='',
+                                   background_color=self.colorformat.bttn_bckgrd_color,
+                                   font_size=self.fontsize.bttn_size)
+        self.close_select.bind(on_press=self.close)
         self.inside.add_widget(self.close_select)
 
-        self.tube_select = Button(text = "[b]SINGLE TUBES[/b]", markup = True, color = colorformat.bttn_fwd_color, 
-        background_normal = '', 
-        background_color = colorformat.bttn_bckgrd_color,
-        font_size = fontsize.bttn_size)
-        self.tube_select.bind(on_press = self.tube)
+        self.tube_select = Button(text="[b]SINGLE TUBES[/b]", markup=True,
+                                  color=self.colorformat.bttn_fwd_color,
+                                  background_normal='',
+                                  background_color=self.colorformat.bttn_bckgrd_color,
+                                  font_size=self.fontsize.bttn_size)
+        self.tube_select.bind(on_press=self.tube)
         self.inside.add_widget(self.tube_select)
 
-        self.plate_select = Button(text = "[b]96-WELL PLATE[/b]", markup = True, color = colorformat.bttn_fwd_color, 
-        background_normal = '', 
-        background_color = colorformat.bttn_bckgrd_color,
-        font_size = fontsize.bttn_size)
-        self.plate_select.bind(on_press = self.plate)
+        self.plate_select = Button(text="[b]96-WELL PLATE[/b]", markup=True,
+                                   color=self.colorformat.bttn_fwd_color,
+                                   background_normal='',
+                                   background_color=self.colorformat.bttn_bckgrd_color,
+                                   font_size=self.fontsize.bttn_size)
+        self.plate_select.bind(on_press=self.plate)
         self.inside.add_widget(self.plate_select)
 
-        self.status = Label(text = f"[b][u]Status[/u]:    Incomplete\n[u]Results Folder[/u]:    N/A[/b]", markup = True, color = colorformat.header_color, font_size = fontsize.subheader_size)
+        self.status = Label(text=f'[b][u]Status[/u]:    Incomplete\n[u]Results Folder[/u]:    N/A[/b]',
+                            markup=True, color=self.colorformat.header_color,
+                            font_size=self.fontsize.subheader_size)
 
         self.add_widget(self.inside1)
         self.add_widget(self.inside2)
-        self.add_widget(Label(text = "[b][i]Bulk Primer Walk for IDT[/i][/b]", markup = True, color = colorformat.header_color, font_size = fontsize.header_size))
+        self.add_widget(Label(text="[b][i]Bulk Primer Walk for IDT[/i][/b]", markup=True,
+                              color=self.colorformat.header_color,
+                              font_size=self.fontsize.header_size))
         self.add_widget(self.status)
-        #self.add_widget(Label(text = f"[b][i]{datetime.date.today()}[/i][/b]", markup = True, color = colorformat.header_color, font_size = fontsize.subheader_size))
         self.add_widget(self.inside3)
         self.add_widget(self.inside4)
         self.add_widget(self.inside)
 
     def close(self, instance):
+        """Button func. for exiting application"""
         if instance:
             App.get_running_app().stop()
             reset()
 
     def tube(self, instance):
+        """Button func. to select tube format"""
         if instance:
             global order_type
             order_type = 'tube'
             exec(open("Primerwalk.py").read(), globals(), globals())
-            os.chdir(main_dir)
+            os.chdir(MAIN_DIR)
             self.status.text = f'[b][u]Status[/u]:    Single Tubes Complete\n[u]Results Folder[/u]:    "{datetime.date.today()}"[/b]'
 
     def plate(self, instance):
+        """Button func. to select plate format"""
         if instance:
             global order_type
             order_type = 'plate'
             exec(open("Primerwalk.py").read(), globals(), globals())
-            os.chdir(main_dir)
+            os.chdir(MAIN_DIR)
             self.status.text = f'[b][u]Status[/u]:    96-Well Plate Complete\n[u]Results Folder[/u]:    "{datetime.date.today()}"[/b]'
 
     def update_rect(self, *args):
-            self.rect.pos = self.pos
-            self.rect.size = self.size
+        """Set button size"""
+        self.rect.pos = self.pos
+        self.rect.size = self.size
+
 
 def reset():
+    """Reset Kivy application upon button click to avoid stack overflow"""
     import kivy.core.window as window
     from kivy.base import EventLoop
     if not EventLoop.event_listeners:
@@ -140,9 +157,14 @@ def reset():
         for cat in Cache._categories:
             Cache._objects[cat] = {}
 
+
 class WorklistGeneratorApp(App):
+    """
+    Build application
+    """
     def build(self):
         return WorklistGenerator()
+
 
 if __name__ == "__main__":
     WorklistGeneratorApp().run()
